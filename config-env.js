7cca85ea3a894070df1af16609485cdef514f08c
Kapskay <165472071+Kapskay@users.noreply.github.com>
2024-06-17 15:13:55 +0200

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -9,7 +9,7 @@ function convertToBool(text, fault = 'true') {
 const username = fs.readFileSync('Kapsykay', 'utf8').trim()
 
 module.exports = {
-    SESSION_ID:  process.env.,    
+    SESSION_ID:  process.env.PRABATH-MD~eW4hAJJ#HFQT2ksz3jrj4t3NmpDe9BqtdCqg1pAg-DABQcA_uw,    
     BOT_NUMBER:  process.env.260776642930,
     GITHUB_USERNAME: Kapskay,
     GITHUB_AUTH_TOKEN: process.env.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,
